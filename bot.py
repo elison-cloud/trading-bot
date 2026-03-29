72d59d2640f450a599e95852e4512e38c04fd708
Elison <elisonquispe8@gmail.com>
2026-03-29 13:10:45 -0500

    bot automatico

--- bot.py
+++ bot.py
@@ -2,45 +2,53 @@ import yfinance as yf
 import pandas as pd
 import ta
 import requests
+import time
+import os
 
-# 🔑 CONFIGURACIÓN TELEGRAM
-TOKEN = "8678597737:AAEv1JBHc5zW4pft7_xgAB5wOxtHP2N1BXI"
-CHAT_ID = "5423327242"
+# 🔑 VARIABLES
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def enviar_mensaje(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-data = yf.download("BTC-USD", period="1mo", interval="1h")
 
-close = data["Close"].squeeze()
+def analizar():
+    data = yf.download("BTC-USD", period="1mo", interval="1h")
 
-data["RSI"] = ta.momentum.RSIIndicator(close).rsi()
-data["EMA20"] = ta.trend.EMAIndicator(close, window=20).ema_indicator()
-data["EMA50"] = ta.trend.EMAIndicator(close, window=50).ema_indicator()
+    close = data["Close"].squeeze()
 
-data = data.dropna()
+    data["RSI"] = ta.momentum.RSIIndicator(close).rsi()
+    data["EMA20"] = ta.trend.EMAIndicator(close, window=20).ema_indicator()
+    data["EMA50"] = ta.trend.EMAIndicator(close, window=50).ema_indicator()
 
-rsi = data["RSI"].iloc[-1]
-ema20 = data["EMA20"].iloc[-1]
-ema50 = data["EMA50"].iloc[-1]
+    data = data.dropna()
 
-if rsi < 30 and ema20 > ema50:
-    print("🚀 COMPRA")
-elif rsi > 70 and ema20 < ema50:
-    print("🔻 VENTA")
-else:
-    print("⏸ ESPERAR")
+    rsi = data["RSI"].iloc[-1]
+    ema20 = data["EMA20"].iloc[-1]
+    ema50 = data["EMA50"].iloc[-1]
 
-print("\nDatos actuales:")
-print(f"RSI: {rsi}")
-print(f"EMA20: {ema20}")
-print(f"EMA50: {ema50}")
-mensaje_final = f"""
-📊 Análisis BTC
+    if rsi < 30 and ema20 > ema50:
+        señal = "🚀 COMPRA"
+    elif rsi > 70 and ema20 < ema50:
+        señal = "🔻 VENTA"
+    else:
+        señal = "⏸ ESPERAR"
+
+    mensaje = f"""
+📊 BTC
 
 RSI: {round(rsi,2)}
 EMA20: {round(ema20,2)}
 EMA50: {round(ema50,2)}
+
+Señal: {señal}
 """
 
-enviar_mensaje(mensaje_final)
+    print(mensaje)
+    enviar_mensaje(mensaje)
+
+# 🔁 LOOP INFINITO
+while True:
+    analizar()
+    time.sleep(3600)  # cada 1 hora
